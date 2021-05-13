f1b43128c3700ec46048e5e9f4788849b582ce7c
soyojuny <soyojuny@gmail.com>
2021-05-13 22:53:58 +0900

    First Commit

---
+++ src/main/java/com/example/money/Dollar.java
@@ -0,0 +1,14 @@
+package com.example.money;
+
+public class Dollar {
+
+    public int amount;
+
+    public Dollar(int amount){
+        this.amount = amount;
+    }
+
+    public Dollar times(int multiplier){
+        return new Dollar(amount * multiplier);
+    }
+}
---
+++ src/test/java/com/example/money/TestMoney.java
@@ -0,0 +1,18 @@
+package com.example.money;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestMoney {
+
+    @Test
+    public void testMultiplication(){
+        Dollar five = new Dollar(5);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
+        product = five.times(3);
+        assertEquals(15, product.amount);
+    }
+}
