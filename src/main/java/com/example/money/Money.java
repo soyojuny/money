363fd20df831590637f71cd1920058974546ff94
soyojuny <soyojuny@gmail.com>
2021-05-15 09:15:32 +0900

    Money Chapter08 Commit

--- src/main/java/com/example/money/Money.java
+++ src/main/java/com/example/money/Money.java
@@ -1,7 +1,17 @@
 package com.example.money;
 
-public class Money {
+abstract class Money {
     protected int amount;
+
+    abstract Money times(int multiplier);
+
+    static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    static Money franc(int amount){
+        return new Franc(amount);
+    }
 
     public boolean equals(Object object){
         Money money = (Money) object;
